0ac27985e3d37eeb540e6ff0188a0c3592ce5fdb
YannCobigo <yann.cobigo@yahoo.com>
2012-10-24 11:29:52 -0700

    Add some command lines

--- hello.cxx
+++ hello.cxx
@@ -3,6 +3,7 @@
 int main(){
 
   std::cout << "hello world!" << std::endl;
+  std::cout << "La vie est belle!" << std::endl;
   std::cout << "Il manque qqch ... Comme la vie est belle" << std::endl;
 
   return 1;
