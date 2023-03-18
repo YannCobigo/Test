506b9940a1732903e3bacd89d90b2c83c1d2730c
Yann Cobigo <yann.cobigo@yahoo.com>
2023-03-18 14:20:53 -0700

    Create myclass.cxx
    state of the art source class

---
+++ myclass.cxx
@@ -0,0 +1,19 @@
+#include "myclass.h"
+
+my_namespace::MyClass::MyClass() {
+    // constructor implementation
+}
+
+my_namespace::MyClass::~MyClass() {
+    // destructor implementation
+}
+
+int 
+my_namespace::MyClass::add(int a, int b) {
+    return a + b;
+}
+
+int 
+my_namespace::MyClass::subtract(int a, int b) {
+    return a - b;
+}
